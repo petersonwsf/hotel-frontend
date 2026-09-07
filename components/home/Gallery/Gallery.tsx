5465d076c08583ff366f254623ad9350cf9ca052
Peterson William <petersondev.profissional@gmail.com>
2026-09-07 11:02:33 -0300

    style: melhorando o design da sessão de galeria

--- components/home/Gallery/Gallery.tsx
+++ components/home/Gallery/Gallery.tsx
@@ -1,11 +1,37 @@
-import GallerySlide from "./GallerySlide";
+import Image from "next/image"
 
 export default async function Gallery() {
     return (
-        <section id="gallery" className="my-[4rem]" aria-label="Galeria de fotos">
-            <h3 className="text-3xl font-semibold font-sans">Fotos</h3>
-            <div className="my-2">
-                <GallerySlide />
+        <section id="gallery" className="my-[4rem] w-full" aria-label="Galeria de fotos">
+            <div className="w-[50%]">
+                <span className="text-[#002179] font-[500] text-sm">NOSSA GALERIA</span>
+                <h3 className="text-3xl font-[650] text-gray-800 font-sans">Momentos Exclusivos no Lúmen Hotel</h3>
+                <p className="font-light text-gray-500">Conheça os detalhes arquitetônicos e recantos de contemplação que tornam nossa propriedade única.</p>
+            </div>
+            <div className="my-2 w-full">
+                <div className="w-full flex gap-3 mt-3">
+                    <div className="w-1/2 h-auto relative">
+                        <Image fill src={`/images/hotel_0.jpg`} alt="Foto do hotel" className="object-cover rounded-lg" />
+                    </div>
+                    <div className="w-1/2 h-auto flex flex-col gap-3">
+                        <div className="w-full flex gap-4">
+                            <div className="h-[200px] w-1/2 relative">
+                                <Image fill src={`/images/hotel_4.webp`} alt="Foto do hotel" className="object-cover rounded-lg" />
+                            </div>
+                            <div className="h-[200px] w-1/2 relative">
+                                <Image fill src={`/images/hotel_3.jpg`} alt="Foto do hotel" className="object-cover rounded-lg" />
+                            </div>
+                        </div>
+                        <div className="w-full flex gap-4">
+                            <div className="h-[200px] w-1/2 relative">
+                                <Image fill src={`/images/hotel_2.jpg`} alt="Foto do hotel" className="object-cover rounded-lg" />       
+                            </div>
+                            <div className="h-[200px] w-1/2 relative">
+                                <Image fill src={`/images/hotel_1.webp`} alt="Foto do hotel" className="object-cover rounded-lg" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
