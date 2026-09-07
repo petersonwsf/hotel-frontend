9acba41ea7c6944e43929d8b369a173b34677d97
Peterson William <petersondev.profissional@gmail.com>
2026-09-07 09:02:30 -0300

    style: melhorando estilo da sessão de quartos (Home)

--- components/home/FeaturedRooms/FeaturedRoomsSlide.tsx
+++ components/home/FeaturedRooms/FeaturedRoomsSlide.tsx
@@ -1,28 +1,53 @@
 "use client";
+
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
+import { Swiper as SwiperClass } from "swiper";
 import CardRoom from "../../ui/CardRoomSlide";
 import { Room } from "@/types/Room.types";
+import { useState } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 
 interface FeaturedRoomsSlideProps {
-    rooms: Room[]
+  rooms: Room[];
 }
 
-export default function FeaturedRoomsSlide({ rooms } : FeaturedRoomsSlideProps) {
-    return (
-        <Swiper
-            modules={[Navigation, Pagination]}
-            slidesPerView={4}
-            speed={1000}
-            navigation
-            loop
-            centeredSlides={true}
+export default function FeaturedRoomsSlide({ rooms }: FeaturedRoomsSlideProps) {
+
+  const [swiperInstance, setSwiperInstance] = useState<SwiperClass | null>(null);
+
+  return (
+    <div>
+      <div className="flex justify-end items-center gap-3 w-full mb-4">
+        <button
+          onClick={() => swiperInstance?.slidePrev()}
+          aria-label="Slide anterior"
+          className="p-2 text-white bg-[#002179] hover:bg-[#001a60] rounded-full transition cursor-pointer active:scale-95"
         >
-            {rooms.map((room) => (
-                <SwiperSlide key={room.id}>
-                    <CardRoom room={room}/>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    )
+          <HiChevronLeft className="text-xl" />
+        </button>
+        <button
+          onClick={() => swiperInstance?.slideNext()}
+          aria-label="Próximo slide"
+          className="p-2 text-white bg-[#002179] hover:bg-[#001a60] rounded-full shadow transition cursor-pointer active:scale-95"
+        >
+          <HiChevronRight className="text-xl" />
+        </button>
+      </div>
+      <Swiper
+        modules={[Pagination]}
+        slidesPerView={4}
+        spaceBetween={20}
+        loop={true}
+        onSwiper={(swiper) => setSwiperInstance(swiper)}
+        className="[&_.swiper-button-next]:hidden [&_.swiper-button-prev]:hidden"
+      >
+        {rooms.map((room) => (
+          <SwiperSlide key={room.id}>
+            <CardRoom room={room} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
--- components/ui/CardRoomSlide.tsx
+++ components/ui/CardRoomSlide.tsx
@@ -1,7 +1,9 @@
 "use client"
-
 import { Room } from "@/types/Room.types"
+import { formatFloor, getRoomCategoryLabel } from "@/utils/formatTextsRooms"
 import { useRouter } from "next/navigation"
+import { CiCalendar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa6";
 
 interface CardRoomSlideProps {
     room: Room
@@ -12,16 +14,24 @@ export default function CardRoomSlide({ room } : CardRoomSlideProps) {
     const router = useRouter()
 
     function handleRedirect() {
-        router.push(`/room/${room.id}`)
+        router.push(`/reservation/${room.id}`)
     }
 
     return (
-        <div className="w-[300px] h-[250px] rounded-tl-[20px] rounded-br-[20px] border overflow-hidden relative cursor-pointer" onClick={handleRedirect}>
-            <img  alt="Imagem do Quarto" src={`${process.env.NEXT_PUBLIC_URL_MINIO}/${room.image}`} className="w-full h-full object-cover" />
-            <div className="absolute inset-0 w-full bg-[rgba(0,0,0,0.5)] flex flex-col justify-end p-5 text-white" >
-                <h3 className="text-xl font-semibold">{`Quarto: ${room.code}`}</h3>
-                <p>{room.customPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
-                <p className="font-light line-clamp-3">{room.amenities}</p>
+        <div className="rounded-lg shadow-xl overflow-hidden cursor-pointer mb-[2rem]" onClick={handleRedirect}>
+            <div className="w-full h-[200px] relative overflow-hidden">
+                <img  alt="Imagem do Quarto" src={`${process.env.NEXT_PUBLIC_URL_MINIO}/${room.image}`} className="w-full h-full object-cover hover:scale-[1.1] duration-[.3s]" />
+                <div className="w-full absolute absolute top-5 flex justify-between px-[.5rem]">
+                    <span className="rounded-xl bg-[#002179] text-white px-[1rem] py-[.1rem] inline-flex items-center gap-2 text-sm">{formatFloor(room.floor)}</span>
+                    <span className="rounded-xl bg-gray-200 px-[1rem] py-[.1rem] inline-flex items-center gap-2 text-sm"><FaStar className="w-3 h-3 text-[#002179]"/> 5.0</span>
+                </div>
+                <span className="rounded-xl bg-gray-200 px-[1rem] py-[.1rem] flex items-center gap-2"><FaStar className="w-3 h-3 text-[#002179]"/> 5.0</span>
+            </div>
+            <div className="w-full p-[1rem] flex flex-col gap-3">
+                <h3 className="text-xl text-[#002179] font-[500]">{getRoomCategoryLabel(room.category)}</h3>
+                <p className="line-clamp-2 break-words font-light">{room.description}</p>
+                <p className="font-light"><span className="font-semibold text-2xl text-[#002179]">{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'brl'}).format(room.customPrice)}</span> / dia</p>
+                <button onClick={handleRedirect} className="bg-[#002179] py-[.5rem] px-[1rem] text-white font-semibold rounded-lg cursor-pointer gap-2 inline-flex justify-center items-center"><CiCalendar /> Reservar quarto</button>
             </div>
         </div>
     )
