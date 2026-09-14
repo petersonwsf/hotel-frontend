577d29fc6031e9456dbf5f2076743e3e91bcfa2a
Peterson William <petersondev.profissional@gmail.com>
2026-09-14 13:33:48 -0300

    change: alterando loading da página de quartos

--- app/(public)/rooms/loading.tsx
+++ app/(public)/rooms/loading.tsx
@@ -1,15 +1,19 @@
 export default function LoadingRoomsList() {
     return (
-        <section id="rooms" aria-label="Quartos disponíveis">
-            <div className="flex justify-center items-start w-7xl m-auto gap-5 my-[4rem]">
+        <div id="rooms" aria-label="Quartos disponíveis" className="my-[4rem] w-7xl m-auto">
+            <div className="w-[60%] mb-[1rem]">
+                <span className="text-[#002179] font-[500] text-sm">COLEÇÃO DE ACOMODAÇÕES</span>
+                <h3 className="text-4xl font-[650] font-sans text-[#002179]">Acomodações & Suítes Exclusivas</h3>
+                <p className="font-light text-gray-500">Encontre o refúgio perfeito nos Jardins. Conforto contemporâneo, isolamento acústico impecável e alta hospitalidade personalizada.</p>
+            </div>
+            <div className="flex justify-center items-start gap-5">
                 <aside aria-label="Filtros" className="bg-gray-300 p-5 w-[30%] h-[500px] rounded-[10px] sticky top-10 animate-pulse"></aside>
-                <div className="w-[70%]">
-                    <h2 className="font-semibold text-3xl">Quartos</h2>
+                <div className="w-full flex flex-col gap-3">
                     {Array.from({ length: 5 }).map((_, index) => (
-                        <div className="flex my-3 p-4 gap-3 bg-gray-300 rounded-[10px] animate-pulse h-[200px]" key={index}></div>
+                        <div className="bg-gray-300 rounded-xl animate-pulse h-[300px]" key={index}></div>
                     ))}
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
